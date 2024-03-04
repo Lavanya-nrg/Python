fe29718a726e04cad1f0d601e504c6e4ce87b517
Lavanya Narang <lavanya.narang@dataverze.ai>
2024-03-04 11:44:00 +0530

    created and accessed a module

---
+++ python_practice_questions/modules/custommodule.py
@@ -0,0 +1,1 @@
+print('this is a custome file for creation of modules')
---
+++ python_practice_questions/modules/date_time_module.py
@@ -0,0 +1,14 @@
+import mymod
+mymod.add(9,8)
+mymod.sub(8,7)
+mymod.mul(6,8)
+from mymod import add 
+import importlib
+
+add(6,8)
+import custommodule
+import custommodule
+importlib.reload(custommodule)
+import custommodule
+
+print(dir(mymod))
